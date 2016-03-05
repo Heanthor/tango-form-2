1078bdcfcb5823b12eec1e996d464a6ac872fdd3
Reed <reedtrevelyan@gmail.com>
2016-03-04 20:55:02 -0500

    class names

---
+++ convertForm.php
@@ -0,0 +1,29 @@
+<?php
+    $classes = array(2 => "Finding sacadas",
+                    3 => "Float like a butterfly",
+                    5 => "Shell Milonga",
+                    8 => "Impossibly small turns",
+                    9 => "Dance between the beats",
+                    11 => "Rhythmic alteraciones",
+                    12 => "Axis unhinged",
+                    14 => "Saturday Yoga",
+                    16 => "Floating walk",
+                    17 => "Uncovering the magic: musicality",
+                    19 => "Tersichore's milonga",
+                    22 => "Silky embrace",
+                    23 => "Wax on wax off",
+                    25 => "Making colgadas effortless",
+                    26 => "Rapid fire footwork",
+                    28 => "Sunday Yoga",
+                    30 => "Ganchos and wraps",
+                    31 => "Finding your power",
+                    33 => "Shellebration milonga");
+
+    public function get_classname($index) {
+        if (!in_array($index, array_keys($classes))) {
+            return "INVALID INDEX";
+        } else {
+            return $classes[$index];
+        }
+    }
+?>
--- printClasses.php
+++ printClasses.php
@@ -4,26 +4,27 @@
     require_once("sqlconnector.php");
     require_once("fileEditor.php");
     require_once("closed.php");
+    require_once("convertForm.php");
 
     $fe = new FileEditor('login-info.txt');
     $credentials = $fe->readFile();
-    
+
     $login = new Credentials("terrapintango.cgpkve9uh8yp.us-east-1.rds.amazonaws.com", $credentials[0], $credentials[1], "tangodb");
     $connector = new SQLConnector($login);
 
     $connector->connect();
-    
+
     $query = "select * from records";
     $result = $connector->retrieve($query);
     echo ("<h1>Class Status</h1>");
     //echo drawTable($result);
-    
+
     for ($i = 0; $i < 33; $i++) {
         if (in_array($i, array_keys($master_class_list))) {
             $lf = $master_class_list[$i];
-            
-            echo "Class ".$i.": [L ".$lf["LEADER"].", F: ".$lf["FOLLOWER"]."]<br>";
+
+            echo get_classname($i).": [L ".$lf["LEADER"].", F: ".$lf["FOLLOWER"]."]<br>";
         }
     }
-    
+
 ?>
