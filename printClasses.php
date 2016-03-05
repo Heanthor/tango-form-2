6d6f31cf5ca2b09368872de574c0ad04e51bf06b
Reed <reedtrevelyan@gmail.com>
2016-03-04 21:37:33 -0500

    new table

--- printClasses.php
+++ printClasses.php
@@ -6,25 +6,26 @@
     require_once("closed.php");
     require_once("convertForm.php");
 
-    $fe = new FileEditor('login-info.txt');
-    $credentials = $fe->readFile();
-
-    $login = new Credentials("terrapintango.cgpkve9uh8yp.us-east-1.rds.amazonaws.com", $credentials[0], $credentials[1], "tangodb");
-    $connector = new SQLConnector($login);
-
-    $connector->connect();
-
-    $query = "select * from records";
-    $result = $connector->retrieve($query);
     echo ("<h1>Class Status</h1>");
     //echo drawTable($result);
 
+    $new_list = array();
     for ($i = 0; $i < 33; $i++) {
         if (in_array($i, array_keys($master_class_list))) {
             $lf = $master_class_list[$i];
 
-            echo get_classname($i).": [L ".$lf["LEADER"].", F: ".$lf["FOLLOWER"]."]<br>";
+            //echo get_classname($i).": [L ".$lf["LEADER"].", F: ".$lf["FOLLOWER"]."]<br>";
+            $new_list[get_classname($i)] = array("LEADER" => $lf["LEADER"], "FOLLOWER" => $lf["FOLLOWER"]);
         }
     }
 
+    $table = "<table border=\"1\">";
+    $table .= "<th>Class</th><th>Leaders</th><th>Followers</th><th>Balance (L - F)</th>";
+    foreach ($new_list as $class => $status) {
+        $table .= "<tr><td><strong>$class</strong></td>";
+        $table .= "<td>".$status['LEADER']."</td><td>".$status['FOLLOWER']."</td>";
+        $table .= "<td>".($status['LEADER'] - $status["FOLLOWER"])."</td></tr>";
+    }
+
+    echo $table;
 ?>
--- support.php
+++ support.php
@@ -4,12 +4,12 @@ function generatePage($body, $title="Example", $stylesheet="") {
     $page = <<<EOPAGE
     <!doctype html>
     <html>
-        <head> 
+        <head>
             <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
             <title>$title</title>
             $stylesheet
         </head>
-                
+
         <body>
             $body
         </body>
@@ -17,5 +17,25 @@ function generatePage($body, $title="Example", $stylesheet="") {
 EOPAGE;
 
     return $page;
+}
+
+function drawTable($result) {
+    $table ="<table border=\"1\"><tr>";
+    foreach(array_keys($result[0]) as $header) {
+        $temp = ucfirst($header);
+        $table .= "<th align=\"center\">$temp</th>";
+    }
+    $table .="</tr>";
+    //Header done
+    foreach(array_values($result) as $value) {
+        $row = "<tr>";
+        foreach(array_values($value) as $entry) {
+            $row .= "<td>$entry</td>";
+        }
+        $row .= "</tr>";
+        $table .= $row;
+    }
+    $table .= "</table>";
+    return $table;
 }
 ?>
